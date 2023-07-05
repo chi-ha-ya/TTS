68391d97535cc2afeec1e389366f15341e79fd34
chi-ha-ya <daniao.v.c@gmail.com>
2023-07-05 20:24:20 +0800

    auto create and run in venv

--- TTS_CN.bat
+++ TTS_CN.bat
@@ -1,1 +1,20 @@
-edge-tts -f "./IN.txt" --write-media "./OUT.mp3" --voice zh-CN-YunxiNeural
+@echo off
+cd /D "%~dp0"
+if exist .venv goto :start
+
+echo "Setup VENV"
+python -m venv .venv
+call .venv\Scripts\activate.bat
+
+echo "Install dependencies"
+pip install edge-tts -i https://mirrors.aliyun.com/pypi/simple/
+goto :run
+
+:start
+echo "Start VENV"
+call .venv\Scripts\activate.bat
+goto :run
+
+:run
+echo "Run TTS_CN"
+python .venv\Scripts\edge-tts.exe -f "./IN.txt" --write-media "./OUT.mp3" --voice zh-CN-YunxiNeural
